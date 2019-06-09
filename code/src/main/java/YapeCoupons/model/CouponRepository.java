3f0dfebee483ba0d0757b3fe89cbed405a06e72e
TISparta <jose.garcia@utec.edu.pe>
2019-06-08 21:54:47 -0500

    Removing developer dependencies

--- code/src/main/java/YapeCoupons/model/CouponRepository.java
+++
@@ -1,7 +0,0 @@
-package YapeCoupons.model;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface CouponRepository extends MongoRepository<Coupon, String> {
-    public Coupon findBy_id (String id_);
-}
