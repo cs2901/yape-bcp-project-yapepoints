07d7798cfa6e7df34989cb41af82e18378644fca
César Salcedo <cesar.salcedo@utec.edu.pe>
2019-06-09 18:26:56 -0500

    Renamed url pathnames

--- code/web/src/main/java/YapeCoupons/LogInController.java
+++ code/web/src/main/java/YapeCoupons/LogInController.java
@@ -6,43 +6,43 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-
 public class LogInController {
     @RequestMapping("/")
     public String Index() {
         return "login";
     }
 
-    @RequestMapping("/Register")
+    @RequestMapping("/register")
     public String Register() {
         return "register";
     }
 
-    @RequestMapping("/Register_local")
+    @RequestMapping("/register_local")
     public String RegisterLocal() {
         return "register_local";
     }
-    @RequestMapping("/Register_personal")
+
+    @RequestMapping("/register_personal")
     public String RegisterPersonal() {
         return "register_personal";
     }
 
-    @RequestMapping("/Register_bank")
+    @RequestMapping("/register_bank")
     public String RegisterBank() {
         return "register_bank";
     }
 
-    @RequestMapping("/Pag_inicio")
+    @RequestMapping("/pag_inicio")
     public String PagInicio() {
         return "pag_inicio";
     }
 
-    @RequestMapping("/Show_cupon")
+    @RequestMapping("/show_cupon")
     public String ShowCupon() {
         return "show_cupon";
     }
 
-    @RequestMapping("/Create_cupon")
+    @RequestMapping("/create_cupon")
     public String CreateCupon() {
         return "create_coupon";
     }
