d3277aed5761f046f8ea5cc240aee4e6da45f2b5
Jhony Angulo <jhony.angulo@utec.edu.pe>
2019-06-23 02:28:51 -0500

    Add title to views

--- code/web/src/main/java/YapeCoupons/controller/Redirect.java
+++ code/web/src/main/java/YapeCoupons/controller/Redirect.java
@@ -9,13 +9,18 @@ import javax.servlet.http.HttpServletRequest;
 // TODO : Rename endpoints. Use meaningful, coherent names.
 @Controller
 public class Redirect {
+    private static final String title = "YapePoints";
     @RequestMapping("/")
-    public String index() { return "index"; }
+    public String index(ModelMap map) {
+        map.addAttribute("title", title);
+        return "index";
+    }
 
     @RequestMapping("/home")
     public String home(final HttpServletRequest req, ModelMap map) {
-        String name = req.getSession().getAttribute("dni").toString();
+        String name = req.getSession().getAttribute("name").toString();
         map.addAttribute("name", name);
+        map.addAttribute("title", title + " - Home");
         return "home";
     }
 
@@ -23,9 +28,15 @@ public class Redirect {
     public String restorePassword() { return "restorePassword"; }
 
     @RequestMapping("/changePassword")
-    public String changePassword() { return "changePassword"; }
+    public String changePassword(ModelMap map) {
+        map.addAttribute("title", title + " - Cambiar constraseña");
+        return "changePassword";
+    }
 
     @RequestMapping("/create_coupon")
-    public String createCouponHTML() { return "create_coupon"; }
+    public String createCouponHTML(ModelMap map) {
+        map.addAttribute("title", title + " - Crear cupón");
+        return "create_coupon";
+    }
 
 }
