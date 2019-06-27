30e8c851a755d84c11b1288e2ad3f4ed8aef3a9f
Jhony Angulo <jhony.angulo@utec.edu.pe>
2019-06-26 19:30:47 -0500

    Update dashboard

--- code/web/src/main/java/YapeCoupons/controller/Coupon.java
+++ code/web/src/main/java/YapeCoupons/controller/Coupon.java
@@ -4,6 +4,7 @@ import YapeCoupons.services.CouponService;
 import com.mongodb.BasicDBObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -39,12 +40,18 @@ public class Coupon {
 
     @RequestMapping(path = "/create_coupon", method = RequestMethod.GET)
     public String createCouponGet(HttpServletRequest request,
-                                  RedirectAttributes redirectAttributes) throws Exception {
+                                  RedirectAttributes redirectAttributes,
+                                  ModelMap map
+                                  ) throws Exception {
         try {
             if (!isLogged(request)) {
                 redirectAttributes.addFlashAttribute("error", "Necesitas logearte");
                 return "redirect:login";
             }
+            map.addAttribute("business_name", "Agregar cupón");
+            map.addAttribute("title", "YapeCupones - Agregar cupón");
+
+
             return "create_coupon";
         } catch (Exception e) {
             redirectAttributes.addFlashAttribute("error", e.getMessage().toString());
