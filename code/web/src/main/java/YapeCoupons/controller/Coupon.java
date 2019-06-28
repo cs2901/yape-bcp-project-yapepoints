0fcfb3d000690009f6d5fb1992003913fe6caa83
César Salcedo <cesar.salcedo@utec.edu.pe>
2019-06-28 10:53:48 -0500

    Added coupon edition route and .html

--- code/web/src/main/java/YapeCoupons/controller/Coupon.java
+++ code/web/src/main/java/YapeCoupons/controller/Coupon.java
@@ -88,12 +88,14 @@ public class Coupon {
         }
     }
 
-    @ResponseBody
     @RequestMapping("/coupon/edit/{id}")
     public String editCouponGet(@PathVariable("id") String id,
+                                  ModelMap map,
                                   HttpServletRequest request,
                                   RedirectAttributes redirectAttributes) throws Exception {
-        return "TODO : Show edit view for coupon with id " + id;
+        map.addAttribute("coupon", coupons.findById(id));
+        
+        return "edit_coupon";
     }
 
     // Las funciones de abajo hasta ahora no se usan para nada
